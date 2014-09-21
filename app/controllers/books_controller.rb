40b065c0e95c2d8584e98ee9d2565999154243b5
msalli <alli.cernoch@gmail.com>
2014-09-21 12:42:29 -0700

    fucccckinngggg login

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
+
 
   def index
     @book = Book.all()
@@ -7,21 +9,43 @@ class BooksController < ApplicationController
 
   def create
    # use amazon_request() to look up books
+   Book.create(book_params)
 
   end
 
   def show
+    @book
+
   end
 
   def update
+    @book.update(book_params)
+
   end
 
   def destroy
+    @book.destroy
+
   end
 
   private
 
-  def set_params
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+
+  def book_params
+    params.require(:book).permit(
+      :title,
+      :author,
+      :description,
+      :lg_img,
+      :med_img,
+      :rating,
+      :tags
+      )
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,49 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :update, :destroy]
 
   def index
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
+    @user = User.create(user_params)
+
+    redirect_to root_path
+
   end
 
   def show
+    @user
+
   end
 
   def update
+    @user.update(user_params)
+
   end
 
   def destroy
+    @user.destroy
+
   end
 
+  private
+
+  def find_user
+    @user = User.find_by_id(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :image_url
+      )
+  end
 
 end
